73482a477820c4eb2baaa76f98b711f9bb25f4c6
kris994 <kristipomo@gmail.com>
2020-08-24 08:55:42 +0200

    Initial Commit

---
+++ DAN_LV_Kristina_Garcia_Francisco/DDL/DDL.sql
@@ -0,0 +1,28 @@
+-- Dropping the tables before recreating the database in the order depending how the foreign keys are placed.
+IF OBJECT_ID('tblPizzaIngredient', 'U') IS NOT NULL DROP TABLE tblPizzaIngredient;
+IF OBJECT_ID('tblPizza', 'U') IS NOT NULL DROP TABLE tblPizza;
+IF OBJECT_ID('tblIngredient', 'U') IS NOT NULL DROP TABLE tblIngredient;
+
+-- Checks if the database already exists.
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'PizzaPanDB')
+CREATE DATABASE PizzaPanDB;
+GO
+
+USE PizzaPanDB
+GO
+
+CREATE TABLE tblIngredient(
+	IngredientID		INT IDENTITY(1,1) PRIMARY KEY 	NOT NULL,
+	IngredientName		NVARCHAR (40)					NOT NULL,
+);
+
+CREATE TABLE tblPizza(
+	PizzaID			INT IDENTITY(1,1) PRIMARY KEY 	NOT NULL,
+	PizzaSize		NVARCHAR (40)					NOT NULL,
+);
+
+CREATE TABLE tblPizzaIngredient(
+	PizzaIngredientID		INT IDENTITY(1,1) PRIMARY KEY 	NOT NULL,
+	PizzaID INT FOREIGN KEY REFERENCES tblPizza(PizzaID) NOT NULL,
+	IngredientID INT FOREIGN KEY REFERENCES tblIngredient(IngredientID)	NOT NULL,
+);
